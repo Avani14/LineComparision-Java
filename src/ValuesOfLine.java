b748afc0ea2ba19d3eba0d627cc5ec39e5d835cc
Avani14 <trivediavani14@gmail.com>
2022-05-31 18:54:03 +0530

    All the remaining Test cases completed

--- src/ValuesOfLine.java
+++ src/ValuesOfLine.java
@@ -1,21 +1,15 @@
 
-public class ValuesOfLine {
-	protected float x1;
-	protected float y1;
-	protected float x2;
-	protected float y2;
+public class ValuesOfLine extends Main{
 	
-	public ValuesOfLine(float x1,float y1,float x2,float y2)
-	{
-		this.x1 = x1;
-		this.x1 = y1;
-		this.x1 = x2;
-		this.x1 = y2;
-		System.out.println("the value of x1 : "+x1);
-		System.out.println("the value of y1 : "+y1);
-		System.out.println("the value of x2 : "+x2);
-		System.out.println("the value of y2 : "+y2);
+	public ValuesOfLine(){
+		System.out.println("the value of x1   "+x1);
+		System.out.println("the value of y1   "+y1);
+		System.out.println("the value of x2   "+x2);
+		System.out.println("the value of y2   "+y2);
+		
 	}
 	
-
 }
+	
+
+
